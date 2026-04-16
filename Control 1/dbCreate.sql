4d2602bd13777e9c4f2c4a32898313d08d8ca47e
Camilo cuero <camilo.cuero@usach.cl>
2026-04-16 19:00:36 -0400

    Ajuste de tablas

--- Control 1/dbCreate.sql
+++ Control 1/dbCreate.sql
@@ -26,83 +26,83 @@ DROP TABLE IF EXISTS Compania CASCADE;
 
 -- Tablas Maestras
 CREATE TABLE Cliente (
-  Cliente_ID INT PRIMARY KEY,
-  Identificador_Cliente VARCHAR(50),
-  Nombre_Cliente VARCHAR(50),
+  Cliente_ID INT PRIMARY KEY Not NULL,
+  Identificador_Cliente VARCHAR(50) Not NULL,
+  Nombre_Cliente VARCHAR(50) Not NULL,
   Correo VARCHAR(50),
   Nacionalidad VARCHAR(50)
 );
 
 CREATE TABLE Compania (
-  Compania_ID INT PRIMARY KEY,
-  Nombre_Compania VARCHAR(50)
+  Compania_ID INT PRIMARY KEY Not NULL,
+  Nombre_Compania VARCHAR(50) Not NULL
 );
 
 CREATE TABLE Modelo (
-  Modelo_ID INT PRIMARY KEY,
-  Nombre_Modelo VARCHAR(50)
+  Modelo_ID INT PRIMARY KEY Not NULL,
+  Nombre_Modelo VARCHAR(50) Not NULL
 );
 
 CREATE TABLE Seccion (
-  Seccion_ID INT PRIMARY KEY,
-  Tipo_Seccion VARCHAR(50)
+  Seccion_ID INT PRIMARY KEY Not NULL,
+  Tipo_Seccion VARCHAR(50) Not NULL
 );
 
 -- Tablas con Relaciones
 CREATE TABLE Empleado (
-  Empleado_ID INT PRIMARY KEY,
-  Compania_ID INT REFERENCES Compania(Compania_ID),
-  Puesto_Empleo VARCHAR(50),
-  Nombre_Empleado VARCHAR(50),
-  Identificador_Empleado VARCHAR(50)
+  Empleado_ID INT PRIMARY KEY Not NULL,
+  Compania_ID INT REFERENCES Compania(Compania_ID) Not NULL,
+  Puesto_Empleo VARCHAR(50) Not NULL,
+  Nombre_Empleado VARCHAR(50) Not NULL,
+  Identificador_Empleado VARCHAR(50) Not NULL 
 );
 
 CREATE TABLE Avion (
-  Avion_ID INT PRIMARY KEY,
-  Compania_ID INT REFERENCES Compania(Compania_ID),
-  Modelo_ID INT REFERENCES Modelo(Modelo_ID),
-  Fecha_Adquisicion TIMESTAMP
+  Avion_ID INT PRIMARY KEY Not NULL,
+  Compania_ID INT REFERENCES Compania(Compania_ID) NOT NULL,
+  Modelo_ID INT REFERENCES Modelo(Modelo_ID) NOT NULL,
+  Fecha_Adquisicion TIMESTAMP NOT NULL
 );
 
 CREATE TABLE Vuelo (
-  Vuelo_ID INT PRIMARY KEY,
-  Avion_ID INT REFERENCES Avion(Avion_ID),
-  Compania_ID INT REFERENCES Compania(Compania_ID),
-  Origen VARCHAR(50),
-  Destino VARCHAR(50),
-  Fecha_Vuelo TIMESTAMP
+  Vuelo_ID INT PRIMARY KEY Not NULL,
+  Avion_ID INT REFERENCES Avion(Avion_ID) NOT NULL,
+  Compania_ID INT REFERENCES Compania(Compania_ID) NOT NULL,
+  Origen VARCHAR(50) Not NULL,
+  Destino VARCHAR(50) Not NULL,
+  Fecha_Vuelo TIMESTAMP Not NULL
 );
 
 CREATE TABLE Pasaje (
-  Pasaje_ID INT PRIMARY KEY,
-  Seccion_ID INT REFERENCES Seccion(Seccion_ID),
-  Cliente_ID INT REFERENCES Cliente(Cliente_ID),
-  Vuelo_ID INT REFERENCES Vuelo(Vuelo_ID)
+  Pasaje_ID INT PRIMARY KEY Not NULL,
+  Seccion_ID INT REFERENCES Seccion(Seccion_ID) Not NULL,
+  Cliente_ID INT REFERENCES Cliente(Cliente_ID) Not NULL,
+  Vuelo_ID INT REFERENCES Vuelo(Vuelo_ID) Not NULL
 );
 
 CREATE TABLE Costo (
-  Costo_ID INT PRIMARY KEY,
-  Pasaje_ID INT REFERENCES Pasaje(Pasaje_ID),
-  Precio DECIMAL(10,2)
+  Costo_ID INT PRIMARY KEY Not NULL,
+  Pasaje_ID INT REFERENCES Pasaje(Pasaje_ID) Not NULL,
+  Precio DECIMAL(10,2) Not NULL
 );
 
 CREATE TABLE Sueldo (
-  Sueldo_ID INT PRIMARY KEY,
-  Empleado_ID INT REFERENCES Empleado(Empleado_ID),
-  Monto_Sueldo DECIMAL(10,2),
-  Fecha_Pago TIMESTAMP
+  Sueldo_ID INT PRIMARY KEY Not NULL,
+  Empleado_ID INT REFERENCES Empleado(Empleado_ID) Not NULL,
+  Monto_Sueldo DECIMAL(10,2) Not NULL,
+  Fecha_Pago TIMESTAMP Not NULL
 );
 
 CREATE TABLE Emp_Vuelo (
-  Emp_Vuelo_ID INT PRIMARY KEY,
-  Vuelo_ID INT REFERENCES Vuelo(Vuelo_ID),
-  Empleado_ID INT REFERENCES Empleado(Empleado_ID)
+  Emp_Vuelo_ID INT PRIMARY KEY Not NULL,
+  Vuelo_ID INT REFERENCES Vuelo(Vuelo_ID) Not NULL,
+  Empleado_ID INT REFERENCES Empleado(Empleado_ID) Not NULL
 );
 
 CREATE TABLE Cliente_Comp (
-  Cliente_Comp_ID INT PRIMARY KEY,
-  Cliente_ID INT REFERENCES Cliente(Cliente_ID),
-  Compania_ID INT REFERENCES Compania(Compania_ID)
+  Cliente_Comp_ID INT PRIMARY KEY Not NULL,
+  Cliente_ID INT REFERENCES Cliente(Cliente_ID) Not NULL,
+  Compania_ID INT REFERENCES Compania(Compania_ID) Not NULL
 );
 
 CREATE TABLE Cliente_Vuelo (
